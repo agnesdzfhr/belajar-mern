b593433af4701e4bc25347f58b9a83979f38eac8
Agnes Diza Fahira <agnesdizaf@gmail.com>
2023-01-04 17:59:12 +0700

    add register
    basic component with hooks and slicing page

---
+++ src/assets/inde.js
@@ -0,0 +1,4 @@
+import RegisterBG from "./image/register-bg.jpg";
+import LoginBg from "./image/login-bg.jpg";
+
+export {RegisterBG, LoginBg};
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { RegisterBG } from '../../assets/inde';
 import './register.css';
+import {Button, Gap, Input, Link} from '../../components'
 
 const Register = () => {
   return (
     <div className='main-page'>
         <div className='left'>
-          <p>Ilustrasi</p>
+          <img src={RegisterBG} className='bg-image' alt='imageBG'/>
         </div>
         <div className='right'>
-          <p>Form Register</p>
+          <p className='title'>Register</p>
+          <Input label="Full Name" placeholder="Full Name"/>
+          <Gap height={18}/>
+          <Input label="Email" placeholder="Email"/>
+          <Gap height={18}/>
+          <Input label="Password" placeholder="Password"/>
+          <Gap height={50}/>
+          <Button title="Register"/>
+          <Gap height={10}/>
+          <Link title="Kembali ke Login"/>
         </div>
     </div>
   )
